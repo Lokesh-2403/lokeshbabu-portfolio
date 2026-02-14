eb6a910c3e8e2f1ea6be1f9ef171212677a2ebfa
Lokesh-2403 <adusumalli3818@gmail.com>
2026-02-15 02:47:15 +0530

    fixed base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lokeshbabu-portfolio/',  // 👈 VERY IMPORTANT
+  base: '/',  // 👈 VERY IMPORTANT
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
